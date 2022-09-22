4f7fe82689b3a6d15ea7f82b36ea58b15fbd5465
Jcortes99 <jcortesro99@gmail.com>
2022-09-22 10:07:29 -0500

    First commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeroesComponent
+  ],
+  exports: [
+    HeroesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesModule { }
